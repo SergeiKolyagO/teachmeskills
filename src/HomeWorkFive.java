9794d9394d23c9ed1c54426e9697a4b8f29c7199
SergeiKolyagO <trus_m@mail.ru>
2023-02-16 15:59:32 +0300

    HomeWork5

---
+++ src/HomeWorkFive.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+
+public class HomeWorkFive {
+    public static void main(String[] args) {
+    getSumNumb();
+    }
+    public static void getSumNumb (){
+        int [][] array = {
+                {1, 2, 5, 2},
+                {4, -4, 4, 8},
+                {3, 4, -1, 2}
+        };
+        System.out.println("введите число на которое хотите увеличить массив: ");
+        int numb = ;
+    }
+}

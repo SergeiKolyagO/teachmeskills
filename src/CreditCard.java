b92a27eaee5dd89cb4315545d64b739ae94b97f3
SergeiKolyagO <trus_m@mail.ru>
2023-02-18 10:45:09 +0300

    HomeWork6

---
+++ src/CreditCard.java
@@ -0,0 +1,20 @@
+public class CreditCard {
+    int accountNumber;
+    int currentSum;
+    public  CreditCard (int cardAccountNumber, int cardCurrentSum){
+        accountNumber = cardAccountNumber;
+        currentSum = cardCurrentSum;
+    }
+    public void addSum (){
+        currentSum += 200;
+
+    }
+    public void getInfoCard(){
+        System.out.println("Карта №: " + accountNumber);
+        System.out.println("текущий счет: "+ currentSum);
+    }
+    public void takeOff(){
+        currentSum -=100;
+
+    }
+}

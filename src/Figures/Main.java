832c57329a5ce978483325cccb629137d7b0a7db
SergeiKolyagO <trus_m@mail.ru>
2023-02-21 21:38:31 +0300

    HomeWork7 Task 1

--- src/Figures/Main.java
+++ src/Figures/Main.java
@@ -4,11 +4,11 @@ public class Main {
     public static void main (String[] args){
         Figure[] one ={ new Rectangle(2,4),
         new Triangle(2,4,5),
-        new Circle(4)};
+        new Circle(4),
+        new Triangle(5,8,11),
+        new Rectangle(5,5)};
         for (Figure el: one) {
             el.getDetails();
         }
-
-
     }
 }

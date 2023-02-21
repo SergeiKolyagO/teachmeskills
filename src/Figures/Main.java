2121d3c450e69937d6ec50a8e564b971a098e340
SergeiKolyagO <trus_m@mail.ru>
2023-02-21 22:29:20 +0300

    HomeWork7 Task 1

--- src/Figures/Main.java
+++ src/Figures/Main.java
@@ -7,8 +7,14 @@ public class Main {
         new Circle(4),
         new Triangle(5,8,11),
         new Rectangle(5,5)};
+        double sumPer = 0;
+        double sumSqr = 0;
         for (Figure el: one) {
             el.getDetails();
+            sumSqr += el.getSquare();
+            sumPer += el.getPerimeter();
         }
+        System.out.println("Сумма периметров: " + sumPer);
+        System.out.println("Сумма площадей: " + sumSqr);
     }
 }

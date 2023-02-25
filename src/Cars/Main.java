f4d81b2ac4191edd0bc2ab34589451cbe7735ee1
SergeiKolyagO <trus_m@mail.ru>
2023-02-25 14:42:36 +0300

    HomeWork7 Task 1 and Task 2

--- src/Cars/Main.java
+++ src/Cars/Main.java
@@ -8,30 +8,30 @@ package Cars;
  */
 public class Main {
     public static void main (String[] args){
-        Cars[] bmw = {new Cars("E60", "black", 2005, 280),
-        new Cars("E39","red", 2000,220),
-        new Cars("E61","green", 2007, 290),
-        new Cars("E90","red", 2001,300)};
-        Cars[] peugeot = { new Cars("406","blue",2000,180),
-        new Cars("306","red", 1998,150),
-        new Cars("307", "white", 2007, 180),
-        new Cars("607","black", 1997, 200)};
+        Cars[] bmw = {new Cars("bmw E60", "black", 2005, 280),
+        new Cars("bmw E39","red", 2000,220),
+        new Cars("bmw E61","green", 2007, 290),
+        new Cars("bmw E90","red", 2001,300)};
+        Cars[] peugeot = { new Cars("peugeot 406","blue",2000,180),
+        new Cars("peugeot 306","red", 1998,150),
+        new Cars("peugeot 307", "white", 2007, 180),
+        new Cars("peugeot 607","black", 1997, 200)};
         Cars old = bmw[0];
         Cars fast = bmw[0];
-        for (Cars cars : bmw) {
-            old = (old.getYearsProduction() >= cars.getYearsProduction()) ? cars : old;
-            fast = (fast.getMaxSpeed() <= cars.getMaxSpeed()) ? cars : fast;
+        for (Cars cars : bmw ) {
+            old = (old.getYearsProduction() > cars.getYearsProduction()) ? cars : old;
+            fast = (fast.getMaxSpeed() < cars.getMaxSpeed()) ? cars : fast;
         }
         for (Cars cars : peugeot) {
-            old = (old.getYearsProduction() >= cars.getYearsProduction()) ? cars : old;
-            fast = (fast.getMaxSpeed() <= cars.getMaxSpeed()) ? cars : fast;
+            old = (old.getYearsProduction() > cars.getYearsProduction()) ? cars : old;
+            fast = (fast.getMaxSpeed() < cars.getMaxSpeed()) ? cars : fast;
         }
-        System.out.println("Самая старая машина: " + old.getColor()
-                + " " + old.getModel()
+        System.out.println("Самая старая машина: " + old.getModel()
+                + " " + old.getColor()
                 + " " + old.getMaxSpeed()
                 + " " + old.getYearsProduction());
-        System.out.println("Самая быстрая машина: " + fast.getColor()
-                + " " + fast.getModel()
+        System.out.println("Самая быстрая машина: " + fast.getModel()
+                + " " + fast.getColor()
                 + " " + fast.getMaxSpeed()
                 + " " + fast.getYearsProduction());
     }

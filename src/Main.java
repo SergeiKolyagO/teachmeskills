66cb9ac1bf5ffcafece9adc5825ca1301ccf51af
SergeiKolyagO <trus_m@mail.ru>
2023-03-12 18:20:05 +0300

    HomeWork10

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,49 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.SimpleTimeZone;
+
 public class Main {
     public static void main (String [] args){
-        String s1 = "5555-eee-1234-xxx-1a2b";
+        String s1 = "5555-qwe-1234-xxx-1a2b";
         getOne(s1); //Вывести на экран в одну строку два первых блока по 4 цифры.
         getTwo(s1); //Вывести на экран номер документа, но блоки из трех букв заменить на ***
+        getFive(s1);
         getSix(s1); //Проверить начинается ли номер документа с последовательности 555.
-        getSeven(s1);
+        getSeven(s1);//Проверить заканчивается ли номер документа на последовательность 1a2b
 
 
     }
     public static void getOne(String s){
-        System.out.println(s.substring(0,4) + s.substring(9,13));
+        String[] name = s.split("-");
+        System.out.println(name[0] + "-" + name[2]);
     }
     public static void getTwo(String s){
-        String s2 = "***";
-        System.out.println((s.replaceAll(s.substring(5,8),s2)
-                .replaceAll(s.substring(14,17),s2)));
+        String[] name = s.split("-");
+        char[] symb1 = name[1].toCharArray();
+        Arrays.fill(symb1, '*');
+        name[1] = new String(symb1);
+        char[] symb2 = name[3].toCharArray();
+        Arrays.fill(symb2, '*');
+        name[3] = new String(symb2);
+        String ns ="";
+        for (String value : name) {
+            ns += value + "-";
+
+        }
+        System.out.println(ns.substring(0,ns.length()-1));
+    }
+    public static void getFive(String s){
+        System.out.println(s.toLowerCase().contains("ABC".toLowerCase()));
     }
     public static void getSix(String s){
         boolean b;
-        b = s.substring(0,3).indexOf("555") != -1;
+        b = s.substring(0, 3).contains("555");
         System.out.println(b);
     }
     public static void getSeven(String s){
         boolean b;
-        b = s.lastIndexOf("1a2b") != -1;
+        b = s.substring(s.length()-4,s.length()).lastIndexOf("1a2b") != -1;
         System.out.println(b);
+
     }
 }
